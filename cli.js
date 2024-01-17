90b4b9a7282123ceec0b953c757d048cfac2079c
Tejas Joshi <tejasjoshi@convertcart.com>
2024-01-17 22:28:35 +0530

    feat: removed redundant file

--- cli.js
+++
@@ -1,7 +0,0 @@
-export const cli = (items) => {
-  console.log("Item\tQuantity\tPrice");
-  console.log("--------------------------------------");
-  items.forEach(({ item, quantity, price }) => {
-    console.log(`${item}\t${quantity}\t\t$${price}`);
-  });
-};
