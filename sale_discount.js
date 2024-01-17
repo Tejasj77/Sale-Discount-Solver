fd447fadf1bdd3d077d2e621aba31ad5f57bc591
Tejas Joshi <tejasjoshi@convertcart.com>
2024-01-17 21:56:03 +0530

    feat: build hashmap from the input

--- sale_discount.js
+++ sale_discount.js
@@ -1,11 +1,26 @@
 import readline from "readline";
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
+
+const priceTable = {
+  milk: { unit: 3.97, saleQuantity: 2, salePrice: 5 },
+  bread: { unit: 2.17, saleQuantity: 3, salePrice: 6 },
+  banana: { unit: 0.99, saleQuantity: 0, salePrice: 0 },
+  apple: { unit: 0.89, saleQuantity: 0, salePrice: 0 },
+};
+
+const hashMap = {};
+
 rl.question(
   "Please enter all the items purchased separated by a comma ",
   (input) => {
     rl.close();
+    input.split(",").map((value) => {
+      hashMap[value.trim()] = hashMap[value.trim()] || 0;
+      hashMap[value.trim()] += 1;
+    });
   }
 );
