525fa498e3d559d2606f329400c3318682ba4442
Tejas Joshi <tejasjoshi@convertcart.com>
2024-01-17 22:12:31 +0530

    feat: README file added

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sale Discount Program
+
+A NodeJS program to find total price and saving based on product discounts.
--- sale_discount.js
+++ sale_discount.js
@@ -22,5 +22,29 @@ rl.question(
       hashMap[value.trim()] = hashMap[value.trim()] || 0;
       hashMap[value.trim()] += 1;
     });
+    let totalPrice = 0;
+    let totNoDiscPrice = 0;
+    Object.entries(hashMap).forEach(([key, value]) => {
+      let totalQuantity = 0;
+      let remQuantity = 0;
+      totNoDiscPrice += value * priceTable[key]["unit"];
+      if (priceTable[key]["saleQuantity"]) {
+        totalQuantity = Math.floor(value / priceTable[key]["saleQuantity"]);
+        remQuantity = value % priceTable[key]["saleQuantity"];
+      } else {
+        remQuantity = value;
+      }
+      let discountPrice = totalQuantity * priceTable[key]["salePrice"];
+      let unitPrice = remQuantity * priceTable[key]["unit"];
+      const finalItemPrice = discountPrice + unitPrice;
+      totalPrice += finalItemPrice;
+      console.log({ key, total: discountPrice + unitPrice });
+      displayItems.push({ item: key, quantity: value, price: finalItemPrice });
+    });
+
+    console.log(`Total price : $${totalPrice.toFixed(2)}`);
+    console.log(
+      `You saved $${(totNoDiscPrice - totalPrice).toFixed(2)} today.`
+    );
   }
 );
